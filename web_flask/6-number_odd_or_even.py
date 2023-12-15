7690606b65099c5381d59a51b2fff147b864bed5
Joshua7792 <j.santiago39@hotmail.com>
2023-12-15 10:25:33 -0400

    fixed

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -62,13 +62,6 @@ def number_template(n):
     """Displays an HTML page only if <n> is an integer."""
     return render_template("5-number.html", n=n)
 
-
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def number_odd_or_even(n):
-    """Displays States whether <n> is odd or even in the body."""
-    return render_template("6-number_odd_or_even.html", n=n)
-
-
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
     """Displays States whether <n> is odd or even in the body"""
