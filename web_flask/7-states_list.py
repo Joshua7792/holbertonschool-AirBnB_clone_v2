1aa196e63ece4fc4dfdac7e5b4f3c2f4440cbc7f
Joshua7792 <j.santiago39@hotmail.com>
2023-12-15 17:52:36 -0400

    fixed again

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,20 +15,20 @@ Routes:
 from flask import Flask, render_template
 from models import storage
 
-app = Flask(__name__)
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Route to display a list of all States."""
-    states = storage.all("State").values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Teardown the current SQLAlchemy session."""
+def teardown(self):
     storage.close()
+
+
+@app.route('/states_list')
+def states_list():
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
