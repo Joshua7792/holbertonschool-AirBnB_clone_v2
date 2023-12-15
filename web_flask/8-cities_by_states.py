a5f8ace8ee57de0a4525d648a8756fe006b65fca
Joshua7792 <j.santiago39@hotmail.com>
2023-12-15 12:00:33 -0400

    another try

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,19 +5,16 @@ Script that starts a Flask web application.
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
 
 
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def states_list():
+def cities_by_states():
     """Display a HTML page with a list of all State objects."""
-    states = storage.all("State").values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=sorted_states)
-
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
 
 @app.teardown_appcontext
 def close_storage(exc):
