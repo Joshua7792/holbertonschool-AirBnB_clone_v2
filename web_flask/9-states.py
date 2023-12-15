8acb6f9f7925b3d31704659d6f473aae6e7164fe
Joshua7792 <j.santiago39@hotmail.com>
2023-12-15 18:26:07 -0400

    fixed

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Display a HTML page with a list of States and their Cities."""
+    """Display a HTML page with a list of all State objects and their cities."""
     states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
@@ -23,5 +23,5 @@ def teardown_db(exception):
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
