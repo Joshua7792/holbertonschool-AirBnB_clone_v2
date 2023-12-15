9ed9e0109062e9107bbb87b317848f2c3456e64b
Joshua7792 <j.santiago39@hotmail.com>
2023-12-15 18:06:33 -0400

    states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,48 +1,37 @@
 #!/usr/bin/python3
 """ This script that starts a Flask web application """
 
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
 
 
 app = Flask(__name__)
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ Route that display a HTML page with a list of states
-    objects sorted by name """
-    state_li = storage.all(State).values()
-    return render_template('7-states_list.html', states=state_li)
-
-
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """ Route that display a HTML page with a list of cities
-    objects sorted by name """
-    city_li = storage.all(State).values()
-    return render_template('8-cities_by_states.html', cities=city_li)
-
-
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def states(id=None):
-    state_dic = storage.all(State)
-    state = None
-    for obj in state_dic.values():
-        if obj.id == id:
-            state = obj
-    return render_template('9-states.html', states=state_dic, id=id,
-                           state=state)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """ Function that removes the current SQL Alchemy Session after each
-    request. """
+def teardown(self):
     storage.close()
 
 
+@app.route('/states')
+def states():
+    states = storage.all(State)
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>')
+def states_id(id):
+    states = storage.all(State).values()
+    state = next((state for state in states if state.id == id), None)
+    if state is not None:
+        cities = sorted(state.cities, key=lambda city: city.name)
+    else:
+        cities = []
+    return render_template('9-states.html', state=state, cities=cities)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
