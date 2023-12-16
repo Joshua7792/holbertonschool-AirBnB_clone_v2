bb20d0f8f594f3da3338808995dcb8e5d2715c1f
Joshua7792 <j.santiago39@hotmail.com>
2023-12-15 22:52:43 -0400

    fixed

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,35 +1,26 @@
-# Your Flask application script
-
-from flask import Flask, render_template
+#!/usr/bin/python3
+""" This module starts a Flask web application """""
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
-from os import environ
+
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_appcontext(self):
+def teardown(self):
     storage.close()
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
+@app.route('/hbnb_filters')
 def hbnb_filters():
-    states = storage.all(State).values()
-    amenities = storage.all(Amenity).values()
-
-    if environ.get('HBNB_TYPE_STORAGE') == 'db':
-        cities = storage.all(City).values()
-    else:
-        cities = {}
-
-    dropdown_data = [{'value': state.id, 'label': state.name}
-                     for state in states]
-
-    return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities, dropdown_data=dropdown_data)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    cities = sorted(storage.all(City).values(), key=lambda city: city.name)
+    amenities = sorted(storage.all(Amenity).values(),
+                       key=lambda amenity: amenity.name)
+    return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities)
